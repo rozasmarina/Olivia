e513578e901fac29de8c6d980b5953c14b4f545b
Ed <ed.colen@gmail.com>
2020-08-31 17:42:19 -0300

    updated place and user models

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,10 @@
 class Place < ApplicationRecord
+  belongs_to :user
+  belongs_to :owner, class_name: "User", optional: true
+  has_many :reviews
+  has_many_attached :photos
+
+  validates :name, :latitude, :longitude, presence: true
+  validates :latitude, uniqueness: { scope: :longitude,
+                                     message: "Este local já existe na base de dados" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_180109) do
+ActiveRecord::Schema.define(version: 2020_08_31_183012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,12 +48,26 @@ ActiveRecord::Schema.define(version: 2020_08_31_180109) do
 
   create_table "places", force: :cascade do |t|
     t.bigint "user_id", null: false
+    t.bigint "owner_id"
     t.string "name"
     t.string "latitude"
     t.string "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["owner_id"], name: "index_places_on_owner_id"
     t.index ["user_id"], name: "index_places_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "place_id", null: false
+    t.text "content"
+    t.integer "rating"
+    t.text "response"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["place_id"], name: "index_reviews_on_place_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,6 +76,18 @@ ActiveRecord::Schema.define(version: 2020_08_31_180109) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "city", null: false
+    t.string "state", null: false
+    t.string "gender", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "phone_number", null: false
+    t.string "username", null: false
+    t.string "latitude"
+    t.string "longitude"
+    t.boolean "business"
+    t.string "cpf"
+    t.string "cnpj"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -71,4 +97,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_180109) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "angels", "users"
   add_foreign_key "places", "users"
+  add_foreign_key "places", "users", column: "owner_id"
+  add_foreign_key "reviews", "places"
+  add_foreign_key "reviews", "users"
 end
