113a0160a85d1ada52d1f4aa261c797ba9e6efca
Deborah Carvalho <deborah.bpc@gmail.com>
2020-09-01 14:51:43 -0300

    started adding policy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+    authorize @user
+  end
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -4,4 +4,8 @@ class UserPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    record.user == user
+  end
 end
