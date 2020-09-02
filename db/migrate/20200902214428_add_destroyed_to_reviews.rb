5118aace41f48ad0701a99e0450bb90da660ca5a
Marina Rozas <rozasmarina@gmail.com>
2020-09-02 18:53:09 -0300

    default value fix

--- db/migrate/20200902214428_add_destroyed_to_reviews.rb
+++ db/migrate/20200902214428_add_destroyed_to_reviews.rb
@@ -1,5 +1,5 @@
 class AddDestroyedToReviews < ActiveRecord::Migration[6.0]
   def change
-    add_column :reviews, :is_destroyed, :boolean
+    add_column :reviews, :is_destroyed, :boolean, default: false
   end
 end
