df722da5fd7c4dccd9c6b9e7d547b2c8493d3fc4
Marina Rozas <rozasmarina@gmail.com>
2020-09-03 13:04:44 -0300

    boolean is_destroyed renamed is_disabled

--- db/migrate/20200902214428_add_destroyed_to_reviews.rb
+++ db/migrate/20200902214428_add_destroyed_to_reviews.rb
@@ -1,5 +1,5 @@
 class AddDestroyedToReviews < ActiveRecord::Migration[6.0]
   def change
-    add_column :reviews, :is_destroyed, :boolean, default: false
+    add_column :reviews, :is_disabled, :boolean, default: false
   end
 end
