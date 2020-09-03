728de9bf144a514373d43b74c511bfd95a467b2e
Marina Rozas <rozasmarina@gmail.com>
2020-09-03 16:37:35 -0300

    twilio wip

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -2,12 +2,3 @@ Twilio.configure do |config|
   config.account_sid = ENV['ACC_SID']
   config.auth_token = ENV['TWILIO_TOKEN']
 end
-
-
-# client = Twilio::REST::Client.new => controller, antes de iterar com os angels
-# - dentro da iteração de angels - 
-# client.messages.create({
-#   from: ENV['TWILIO_PHONE'],
-#   to: "+5511980665313", => angel number
-#   body: 'Hello there! This is a test'
-# })
