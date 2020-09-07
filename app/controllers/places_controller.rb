41de4ac5993b7388b7a72d7a9138c3c81cca4f6e
Ed <ed.colen@gmail.com>
2020-09-07 20:42:20 -0300

    Implemented default avatars, places photo upload

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -55,7 +55,7 @@ class PlacesController < ApplicationController
   private
 
   def place_params
-    params.require(:place).permit(:name, :latitude, :longitude, :address)
+    params.require(:place).permit(:name, :address, :photo)
   end
 
   def set_place
