2514fecbe023807b7a20b9d34fd5e1b424bbcbc8
Deborah Carvalho <deborah.bpc@gmail.com>
2020-09-08 16:40:38 -0300

    Minor fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -37,8 +37,8 @@ import { formReview } from '../components/new_review_form.js';
 document.addEventListener('turbolinks:load', () => {
     // Call your functions here, e.g:
     // initSelect2();
-    initMapbox();
     getRating();
+    initMapbox();
     formBusiness();
     formReview();
 });
