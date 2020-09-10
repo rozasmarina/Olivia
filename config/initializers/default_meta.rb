700f21ae3f070f2d3192cb6348c2614c48c79b1b
Marina Rozas <rozasmarina@gmail.com>
2020-09-10 15:02:51 -0300

    yaml load

--- config/initializers/default_meta.rb
+++ config/initializers/default_meta.rb
@@ -1,2 +1,3 @@
 # Initialize default meta tags.
-DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.yml"))
+DEFAULT_META = YAML.load_file(File.join(File.dirname("config/meta.yml"), 'meta.yml'))
+# YAML.load_file(Rails.root.join("config/meta.yml"))
