24ed29522ec53d23cb267c8ab70c3a406b34189e
Ed <ed.colen@gmail.com>
2020-09-11 11:05:59 -0300

    added notification controller

---
+++ app/controllers/notifications/notifications_controller.rb
@@ -0,0 +1,25 @@
+module Notifications
+  class NotificationsController < Notifications::ApplicationController
+    def index
+      @notifications = notifications.includes(:actor).order('id desc').page(params[:page])
+
+      unread_ids = @notifications.reject(&:read?).select(&:id)
+      Notification.read!(unread_ids)
+
+      @notification_groups = @notifications.group_by { |note| note.created_at.to_date }
+    end
+
+    def clean
+      notifications.delete_all
+      redirect_to notifications_path
+    end
+
+    private
+
+    def notifications
+      raise 'You need require user login for /notifications page.' unless current_user
+
+      Notification.where(user_id: current_user.id)
+    end
+  end
+end
